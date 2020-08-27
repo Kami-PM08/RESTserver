47d45ff018239929c9d46aaeaf46fe263ad94a71
Kami-PM08 <37862210+Kami-PM08@users.noreply.github.com>
2020-08-27 17:01:15 -0500

    MongoDB environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/coffee';
 } else {
-    urlDB = 'mongodb+srv://Momonga:Qs3Me7IsesQWtMgw@cluster0.ornbw.mongodb.net/coffee';
+    urlDB = process.env.MONGODB_URL;
 }
 
 process.env.URLDB = urlDB;
